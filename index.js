7c5c78a6893f8430e51445e26608f761846a8d4d
UrielBadilloG <ubadillog1600@alumno.ipn.mx>
2023-07-01 14:04:19 -0600

    cambios para hackathon

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 const axios = require("axios");
 const express = require("express");
 const dotenv = require("dotenv");
-
+const cors = require("cors");
 // Cargar las variables de entorno desde el archivo .env
 dotenv.config();
+
 
 // Leer la clave de API de OpenAI desde las variables de entorno
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
@@ -22,10 +23,11 @@ async function callOpenAI(prompt) {
       const data = {
         "prompt": `${prompt}`,
         // "prompt": `Dime 3 palabras relacionadas con: gomitas`,
-        "max_tokens": 10,
+        "max_tokens": 100,
         "n": 5,
         "stop": null,
         "temperature": 0.5,
+        
         "top_p": 1
       };
   
@@ -47,10 +49,50 @@ const app = express();
 // Permitir que el servidor maneje solicitudes con cuerpos JSON
 app.use(express.json());
 
-// Definir un endpoint POST en la ruta "/openai"
-app.post("/openai", async (req, res) => {
+// Agregar el middleware de CORS
+app.use(cors());
+
+app.post("/exercice", async (req, res) => {
+  console.log(req.body.prompt)
   // Leer el "prompt" del cuerpo de la solicitud
-  const prompt = req.body.prompt;
+  const prompt = 'Dame unicamente el resultado del siguiente codigo:'+req.body.prompt;
+
+  // Llamar a la API de OpenAI con el "prompt" y obtener la respuesta
+  const response = await callOpenAI(prompt);
+
+  // Devolver la respuesta en formato JSON
+  res.json({ response });
+});
+
+app.post("/retro", async (req, res) => {
+  console.log(req.body.prompt)
+  // Leer el "prompt" del cuerpo de la solicitud
+  const prompt = 'Explicame de manera detallada como mejorar este codigo:'+req.body.prompt;
+
+  // Llamar a la API de OpenAI con el "prompt" y obtener la respuesta
+  const response = await callOpenAI(prompt);
+
+  // Devolver la respuesta en formato JSON
+  res.json({ response });
+});
+
+app.post("/compilar", async (req, res) => {
+  console.log(req.body.prompt)
+  // Leer el "prompt" del cuerpo de la solicitud
+  const prompt = 'Verifica  este codigo python si esta bien identado y es un codigo si se peude compilar, si no tiene sentido el codigo y  si no cumple con cualquiera de las dos, dime "400", si cumple dime "200", unicamente dime 200 o 400:'+req.body.prompt;
+
+  // Llamar a la API de OpenAI con el "prompt" y obtener la respuesta
+  const response = await callOpenAI(prompt);
+
+  // Devolver la respuesta en formato JSON
+  res.json({ response });
+});
+
+
+app.post("/porcentaje", async (req, res) => {
+  console.log(req.body.prompt)
+  // Leer el "prompt" del cuerpo de la solicitud
+  const prompt = 'Unicamente dame de respuesta un porcentaje del 1-90 que tan optimo es el codigo en python, se estricto, no me pongas mas texto mas que el porcentaje: '+req.body.prompt;
 
   // Llamar a la API de OpenAI con el "prompt" y obtener la respuesta
   const response = await callOpenAI(prompt);
@@ -60,7 +102,7 @@ app.post("/openai", async (req, res) => {
 });
 
 // Definir el puerto en el que escuchará el servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 7000;
 
 // Iniciar el servidor en el puerto definido
 app.listen(PORT, () => {
