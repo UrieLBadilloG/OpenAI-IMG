0193e5bf3343fac235d2476c185ef2b4aa856bef
UrielBadilloG <ubadillog1600@alumno.ipn.mx>
2023-10-18 16:48:04 -0600

    first commit

--- index.js
+++ index.js
@@ -52,10 +52,10 @@ app.use(express.json());
 // Agregar el middleware de CORS
 app.use(cors());
 
-app.post("/exercice", async (req, res) => {
+app.post("/texto", async (req, res) => {
   console.log(req.body.prompt)
   // Leer el "prompt" del cuerpo de la solicitud
-  const prompt = 'Dame unicamente el resultado del siguiente codigo:'+req.body.prompt;
+  const prompt = 'hola, dame 3 palabras relacionadas a:'+req.body.prompt;
 
   // Llamar a la API de OpenAI con el "prompt" y obtener la respuesta
   const response = await callOpenAI(prompt);
@@ -64,42 +64,6 @@ app.post("/exercice", async (req, res) => {
   res.json({ response });
 });
 
-app.post("/retro", async (req, res) => {
-  console.log(req.body.prompt)
-  // Leer el "prompt" del cuerpo de la solicitud
-  const prompt = 'Explicame de manera detallada como mejorar este codigo:'+req.body.prompt;
-
-  // Llamar a la API de OpenAI con el "prompt" y obtener la respuesta
-  const response = await callOpenAI(prompt);
-
-  // Devolver la respuesta en formato JSON
-  res.json({ response });
-});
-
-app.post("/compilar", async (req, res) => {
-  console.log(req.body.prompt)
-  // Leer el "prompt" del cuerpo de la solicitud
-  const prompt = 'Verifica  este codigo python si esta bien identado y es un codigo si se peude compilar, si no tiene sentido el codigo y  si no cumple con cualquiera de las dos, dime "400", si cumple dime "200", unicamente dime 200 o 400:'+req.body.prompt;
-
-  // Llamar a la API de OpenAI con el "prompt" y obtener la respuesta
-  const response = await callOpenAI(prompt);
-
-  // Devolver la respuesta en formato JSON
-  res.json({ response });
-});
-
-
-app.post("/porcentaje", async (req, res) => {
-  console.log(req.body.prompt)
-  // Leer el "prompt" del cuerpo de la solicitud
-  const prompt = 'Unicamente dame de respuesta un porcentaje del 1-90 que tan optimo es el codigo en python, se estricto, no me pongas mas texto mas que el porcentaje: '+req.body.prompt;
-
-  // Llamar a la API de OpenAI con el "prompt" y obtener la respuesta
-  const response = await callOpenAI(prompt);
-
-  // Devolver la respuesta en formato JSON
-  res.json({ response });
-});
 
 // Definir el puerto en el que escuchará el servidor
 const PORT = process.env.PORT || 7000;
